888eaf35aba0630f6c7f71fc25cad9b537a8d412
kevinbazira <kvnbzr@gmail.com>
2023-02-13 15:11:22 +0400

    added route that serves index.html

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const express = require("express");
 // instantiate express as app
 const app = express();
 
+// route for "/" path that returns index.html
+app.use(express.static("./"));
+
 // start express server on port 3000 and print message to console
 app.listen(3000, ()=>{
     console.log("App started on 3000 ");
