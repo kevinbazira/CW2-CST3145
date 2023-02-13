6ddb67742dd87934fe84800c829b447af1bdcb32
kevinbazira <kvnbzr@gmail.com>
2023-02-13 15:07:09 +0400

    added feature to start express server

--- server.js
+++ server.js
@@ -1,27 +1,10 @@
+// import express node module
 const express = require("express");
 
+// instantiate express as app
 const app = express();
 
-// get route for "/lessons" path that returns lessons JSON object
-app.get("/lessons", (request, response, next)=>{
-    const lessons = [
-        {'topic': 'math', 'location': 'Hendon', 'price' : 100},
-        {'topic': 'math', 'location': 'Colindale', 'price' : 80},
-        {'topic': 'math', 'location': 'Brent Cross', 'price' : 90},
-        {'topic': 'math', 'location': 'Golders Green', 'price' : 120},
-    ];
-    response.send(lessons);
-});
-
-// get route for "/user" path that returns user details
-app.get("/user", (request, response, next)=>{
-    const user = {'email': 'user@email.com', 'password': 'mypassword'};
-    response.send(user);
-});
-
-// route for "/" path that returns index.html
-app.use(express.static("./"));
-
+// start express server on port 3000 and print message to console
 app.listen(3000, ()=>{
     console.log("App started on 3000 ");
 });
